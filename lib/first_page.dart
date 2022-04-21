4921dbb6901a4c108c9ad80f4db475ba7c5ba440
kuno <ayana.kuno@mixi.co.jp>
2022-04-21 19:33:22 +0900

    次の画面へ進む

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,3 +1,4 @@
+import 'package:beginner_training_flutter/second_page.dart';
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatelessWidget {
@@ -5,6 +6,28 @@ class FirstPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("First")));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("First"),
+            IconButton(
+              onPressed: () {
+                // 画面遷移には Navigator を使う
+                Navigator.push(
+                  context,
+                  // 次の画面を MaterialPageRoute に渡して Route を作り，それを表示させてる
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const SecondPage(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.arrow_forward),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
