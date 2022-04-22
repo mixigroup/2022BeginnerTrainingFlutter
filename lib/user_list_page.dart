597374dd4309ff8f6f5baf3e0de6f9223fc5dec3
kuno <ayana.kuno@mixi.co.jp>
2022-04-22 16:18:21 +0900

    ユーザ一覧を呼んでみよう！

---
+++ lib/user_list_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:graphql/client.dart';
+
+class UserListPage extends StatelessWidget {
+  const UserListPage({Key? key}) : super(key: key);
+
+  // クエリ呼ぶメソッド生やすよ！
+  void callQuery() async {
+    // 接続先
+    final _httpLink =
+        HttpLink("https://serene-garden-89220.herokuapp.com/query");
+    // Graphql のクライアントを準備
+    final GraphQLClient client =
+        GraphQLClient(link: _httpLink, cache: GraphQLCache());
+
+    // これがユーザ一覧を取得するための Query を呼ぶやつ
+    // query <<名付けたクエリ名>> { <<呼びたい Query>> } って感じで書く
+    const String getUserListQuery = r'''
+      query userListPage {
+        userList {
+          name
+          id
+        }
+      }
+    ''';
+    // 例えば『リストページではユーザリストと会社リストの情報が欲しい！』ってときに
+    // ▼こんな感じで名付けたクエリの中で複数の Query を呼ぶことができる！
+    // （実際カンパニーリストクエリはAPIにないからこのコードは動かないです🙇🏻‍♀️）
+    // const String getUserListQuery = r'''
+    //   query ListPageDeYobuQuery {
+    //     userList {
+    //       name
+    //       id
+    //     }
+    //     companyList {
+    //       name
+    //       id
+    //     }
+    //   }
+    // ''';
+
+    // ここで上で書いたクエリを graphql で読み込めるものに変換！
+    final QueryOptions options = QueryOptions(
+      document: gql(getUserListQuery),
+      // 今回はまだ使わないのでコメントアウト
+      // variables: <String, dynamic>{
+      //   'nRepositories': nRepositories,
+      // },
+    );
+
+    // 上で options に変換したクエリを最初に作ったクライアントに乗せて叩く！
+    // レスポンスが届くまで時間がかかるので await してちょっと待ってあげる
+    // 💡 await を使うためにはメソッドに async をつけてあげる必要ある
+    final QueryResult result = await client.query(options);
+
+    // 返ってきた result がエラーだったらログに書く
+    if (result.hasException) {
+      debugPrint("エラーだった：" + result.exception.toString());
+    }
+
+    // 返ってきた result をログに書く！
+    // でも多分見にくいのでターミナルで v 押して devtool の network で見てみよう
+    debugPrint(result.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 上で実装したクエリ呼ぶやつ呼んであげる
+    callQuery();
+
+    // 表示は適当に…
+    return const Scaffold(
+      body: Center(child: Text("うなうな")),
+    );
+  }
+}
