fe32d62ac0cf4f9d417dfbab75501b5d7d419ef8
kuno <ayana.kuno@mixi.co.jp>
2022-04-25 16:46:53 +0900

    ユーザ登録をしてみよう！

--- lib/user_list_page.dart
+++ lib/user_list_page.dart
@@ -8,6 +8,12 @@ class UserListPage extends StatefulWidget {
   State<StatefulWidget> createState() => UserListPageState();
 }
 
+// 接続先
+final _httpLink = HttpLink("https://serene-garden-89220.herokuapp.com/query");
+// Graphql のクライアントを準備
+final GraphQLClient client =
+    GraphQLClient(link: _httpLink, cache: GraphQLCache());
+
 class UserListPageState extends State<UserListPage> {
   // State 宣言！
   List users = [];
@@ -15,13 +21,6 @@ class UserListPageState extends State<UserListPage> {
 
   // クエリ呼ぶメソッド生やすよ！
   void callQuery() async {
-    // 接続先
-    final _httpLink =
-        HttpLink("https://serene-garden-89220.herokuapp.com/query");
-    // Graphql のクライアントを準備
-    final GraphQLClient client =
-        GraphQLClient(link: _httpLink, cache: GraphQLCache());
-
     // これがユーザ一覧を取得するための Query を呼ぶやつ
     // query <<名付けたクエリ名>> { <<呼びたい Query>> } って感じで書く
     // ''' で生のテキストとして認識される（改行やタブも認識）<= row string っていう
@@ -51,6 +50,9 @@ query userListPage {
 
     // ここで上で書いたクエリを graphql で読み込めるものに変換！
     final QueryOptions options = QueryOptions(
+      // キャッシュを自動で持ってくれるけどユーザ一覧をキャッシュしちゃうと
+      // ユーザ登録したあとにユーザ一覧呼んでも登録したユーザが反映されないのでキャッシュなしで
+      fetchPolicy: FetchPolicy.noCache,
       document: gql(getUserListQuery),
       // 今回はまだ使わないのでコメントアウト
       // variables: <String, dynamic>{
@@ -90,44 +92,183 @@ query userListPage {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // ボタンが押されたらクエリを呼ぶ！
-            IconButton(
-                onPressed: () {
-                  // ボタン押したら loading を true に
-                  setState(() {
-                    loading = true;
-                  });
-                  callQuery();
-                },
-                icon: const Icon(Icons.refresh)),
-            // loading フラグが true の時は Loading... と表示
-            if (loading) ...[
-              const Text("Loading…"),
-            ],
-            // users が null じゃないとき（クエリでデータが返ってきたとき）はそれを表示！
-            if (users.isNotEmpty) ...[
-              SizedBox(
-                height: 350,
-                child: ListView.builder(
-                  itemCount: users.length,
-                  itemBuilder: (_, index) {
-                    return Column(
-                      children: [
-                        Text(users[index]["name"]),
-                        Text(users[index]["id"]),
-                        const Text("----"),
-                      ],
-                    );
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // ボタンが押されたらクエリを呼ぶ！
+              IconButton(
+                  onPressed: () {
+                    // ボタン押したら loading を true に
+                    setState(() {
+                      loading = true;
+                    });
+                    callQuery();
                   },
+                  icon: const Icon(Icons.refresh)),
+              // loading フラグが true の時は Loading... と表示
+              if (loading) ...[
+                const Text("Loading…"),
+              ],
+              // users が null じゃないとき（クエリでデータが返ってきたとき）はそれを表示！
+              if (users.isNotEmpty) ...[
+                SizedBox(
+                  height: 350,
+                  child: ListView.builder(
+                    itemCount: users.length,
+                    itemBuilder: (_, index) {
+                      return Column(
+                        children: [
+                          Text(users[index]["name"]),
+                          Text(users[index]["id"]),
+                          const Text("----"),
+                        ],
+                      );
+                    },
+                  ),
                 ),
-              ),
+              ],
+              // ここにユーザ登録するためのテキストフィールドと表示するウィジェットを増やす！
+              const UserRegister(),
             ],
-          ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class UserRegister extends StatefulWidget {
+  const UserRegister({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => UserRegisterwState();
+}
+
+class UserRegisterwState extends State<UserRegister> {
+  // 今回はユーザ登録に３つ情報が必要！
+  String userId = '';
+  String name = '';
+  String password = '';
+
+  void callMutation() async {
+    // 今度は query じゃなくて mutation
+    // 引数の受け取り方
+    // 名付けたミューテーション名（createUserMutation）のところで引数を宣言（今回は String! 型の inputName や inputUserId を使うで〜って宣言）
+    // ▲ここの型は graphql のスキーマに合わせてあげてください
+    // 呼び出すミューテーション（cteateUser）のところで引数を渡してあげる（今回は name に inputName を渡してあげてる）
+    // $ がついてると生のテキストから変数になれる！
+    const String createUserMutation = r'''
+mutation UserRegister($inputName: String!, $inputUserId: String!, $inputPassword: String!) {
+  createUser(name: $inputName, id: $inputUserId, password: $inputPassword) {
+    name
+    id
+  }
+}
+''';
+
+    final MutationOptions options = MutationOptions(
+      document: gql(createUserMutation),
+      // ここで引数を渡してあげる！
+      // 上のミューテーション文で定義した inputName に State の name を渡してあげよう！
+      variables: <String, dynamic>{
+        'inputName': name,
+        'inputUserId': userId,
+        'inputPassword': password,
+      },
+    );
+    // .mutate でミューテーション実行！！
+    final QueryResult result = await client.mutate(options);
+
+    // もしエラーを取得したら
+    final exception = result.exception;
+    if (exception != null) {
+      debugPrint("エラーだった：" + result.exception.toString());
+      // ダイアログを出してあげる！
+      setState(() {
+        showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: const Text("ユーザ登録失敗🥺"),
+              content: Text(exception.graphqlErrors.toString()),
+              actions: [
+                TextButton(
+                  child: const Text("OK"),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            );
+          },
+        );
+      });
+    }
+
+    // もしユーザ登録が成功していたら id と name が返ってくるのでデータが存在する
+    // 成功してもダイアログ出してあげよう！
+    final data = result.data;
+    if (data != null) {
+      showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: const Text("ユーザ登録成功！🌟"),
+            actions: [
+              TextButton(
+                child: const Text("OK"),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: TextField(
+            decoration: const InputDecoration(hintText: "名前を入力"),
+            onChanged: (value) {
+              setState(() {
+                name = value;
+              });
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: TextField(
+            decoration: const InputDecoration(hintText: "IDを入力"),
+            onChanged: (value) {
+              setState(() {
+                userId = value;
+              });
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: TextField(
+            decoration: const InputDecoration(hintText: "パスワードを入力"),
+            // obscureText を true にするとよくあるパスワード隠しモードになる
+            obscureText: true,
+            onChanged: (value) {
+              setState(() {
+                password = value;
+              });
+            },
+          ),
+        ),
+        IconButton(
+          onPressed: callMutation,
+          icon: const Icon(Icons.get_app),
+        ),
+      ],
     );
   }
 }
