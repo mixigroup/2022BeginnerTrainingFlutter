ef6bf9f311fb0825b27b5ae8852e673b3a6e658c
kuno <ayana.kuno@mixi.co.jp>
2022-04-26 23:35:59 +0900

    補足：Column の中に GridView

--- lib/sample_page.dart
+++ lib/sample_page.dart
@@ -13,10 +13,53 @@ class SamplePage extends StatelessWidget {
         body: Column(
           // MainAxisSize.min をつけることで要素の大きさに留まる
           mainAxisSize: MainAxisSize.min,
-          children: const [
-            Text("hoge"),
-            Text("fuga"),
-            Text("piyo"),
+          children: [
+            const Text("hoge"),
+            const Text("fuga"),
+            const Text("piyo"),
+            // Column の中に GridView を入れると GridView は縦に広がろうとするためエラーになる
+            // 解決策① GridView の高さを制限してあげる
+            // SizedBox(
+            //   height: 300,
+            //   child: GridView.count(
+            //     crossAxisCount: 3,
+            //     children: [
+            //       for (var i = 0; i < 9; i++)
+            //         const Padding(
+            //           padding: EdgeInsets.all(8.0),
+            //           child: ColoredBox(color: Colors.pink),
+            //         )
+            //     ],
+            //   ),
+            // ),
+            // 解決策② Expanded を使用する
+            // Expanded は親のサイズの隙間を埋めるもの
+            // Expanded(
+            //   child: GridView.count(
+            //     // shrinkWrap: true,
+            //     crossAxisCount: 3,
+            //     children: [
+            //       for (var i = 0; i < 9; i++)
+            //         const Padding(
+            //           padding: EdgeInsets.all(8.0),
+            //           child: ColoredBox(color: Colors.pink),
+            //         )
+            //     ],
+            //   ),
+            // ),
+            // 解決策③ shrinkWrap を true にする
+            // 要素の大きさに留まる
+            GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 3,
+              children: [
+                for (var i = 0; i < 9; i++)
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ColoredBox(color: Colors.pink),
+                  )
+              ],
+            ),
           ],
         ));
   }
